56271a7645bc745fd2250eefcf705a193ea3d33b
Hasan Atık <64609825+hasantk@users.noreply.github.com>
2024-01-05 16:58:17 +0300

    Food_APP_ReactjsProject

---
+++ src/components/CategoryMenu.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import FoodData from "../Data/FoodData";
+import { useDispatch, useSelector } from "react-redux";
+import { setCategory } from "../redux/slices/CategorySlice";
+
+const CategoryMenu = () => {
+  const [categories, setCategories] = useState([]);
+
+  const ListUniqueCategories = () => {
+    const uniqueCategories = [
+      ...new Set(FoodData.map((food) => food.category)),
+    ];
+    setCategories(uniqueCategories);
+  };
+
+  useEffect(() => {
+    ListUniqueCategories();
+  }, []);
+
+  const dispatch = useDispatch();
+  const selectedCategory = useSelector((state) => state.category.category);
+
+  return (
+    <div className="ml-6">
+      <h3 className="text-xl font-semibold">Find the best food</h3>
+      <div className="my-5 flex gap-3 overflow-x-scroll scroll-smooth lg:overflow-x-hidden">
+        <button
+          onClick={() => dispatch(setCategory("All"))}
+          className={`px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white ${ selectedCategory === "All" && "bg-green-500 text-white"}`}
+        >
+          All
+        </button>
+        {categories.map((category, index) => {
+          return (
+            <button
+              onClick={() => dispatch(setCategory(category))}
+              key={index}
+              className={`px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white ${ selectedCategory === category && "bg-green-500 text-white"}`}
+            >
+              {category}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default CategoryMenu;
